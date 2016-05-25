0eea9b456b64eb5bbe5d69e88650cc103cfb6b58
Nick McIntosh <nick@nickmcintosh.net>
2016-05-25 13:48:19 +1000

    Initial commit

--- package.js
+++ package.js
@@ -1,29 +1,30 @@
 /* eslint-disable prefer-arrow-callback */
 /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
 Package.describe({
-  summary: 'Login service for Facebook (2.4) accounts (works with cordova)',
+  summary: 'Login service for Facebook accounts using a directly-passed accessToken',
   version: '0.1.12',
-  git: 'https://github.com/heysailor/accounts-facebook.git',
+  git: 'https://github.com/heysailor/accounts-facebook-accesstoken.git',
   author: 'Nick McIntosh',
-  name: 'heysailor:accounts-facebook-cordova',
+  name: 'heysailor:accounts-facebook-accesstoken',
 });
 
 Package.onUse(function config(api) {
   api.use([
-    'ecmascript@0.1.6',
-    'accounts-base@1.2.2',
-    'accounts-oauth@1.1.8',
-    'heysailor:facebook@1.2.6',
-    'service-configuration@1.0.5',
-    'http@1.1.1',
-    'underscore@1.0.4',
-  ], ['client', 'server']);
+    'ecmascript@0.4.3',
+    'http',
+    'accounts-base@1.2.7',
+    'facebook',
+    'check@1.2.1',
+    'oauth@1.1.10',
+    'service-configuration@1.0.9'
+  ], 'server');
 
   // Export Accounts (etc) to packages using this one.
   api.imply([
     'accounts-base',
-  ], ['client', 'server']);
+    'http',
+    'oauth'
+  ], ['server']);
 
-  api.add_files('facebook_server.js', 'server');
-  api.add_files('facebook.js');
+  api.add_files('facebook.js', 'server');
 });
